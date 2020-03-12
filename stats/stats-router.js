e850336563778f5b424e432f49d33a02d19baca1
cahaug <caleahaug@gmail.com>
2020-03-12 02:43:48 -0700

    should fix the timing issue

--- stats/stats-router.js
+++ stats/stats-router.js
@@ -7,12 +7,12 @@ statsRouter.get('/', async (req, res) => {
     const refURL = req.query.ref
     const entryId = req.query.eid
     const date = new Date().toISOString();
-    const dy = date.slice(9, 10)
-    const mo = date.slice(6, 7)
+    const dy = date.slice(8, 10)
+    const mo = date.slice(5, 7)
     const yr = date.slice(0, 4)
-    const hr = date.slice(12, 13)
-    const mn = date.slice(15, 16)
-    const sc = date.slice(18, 19)
+    const hr = date.slice(11, 13)
+    const mn = date.slice(14, 16)
+    const sc = date.slice(17, 19)
     const stat = { entryId, dy, mo, yr, hr, mn, sc }
     console.log('stat', stat)
     return logAClick(stat)
