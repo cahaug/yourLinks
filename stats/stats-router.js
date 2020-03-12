9110278118b428d07f1b7c66d36727852e6579fa
cahaug <caleahaug@gmail.com>
2020-03-12 03:12:01 -0700

    finally will redirect

--- stats/stats-router.js
+++ stats/stats-router.js
@@ -18,7 +18,7 @@ statsRouter.get('/', async (req, res) => {
     return logAClick(stat)
     .then(result => {
         // return this.props.history.push(`${refURL}`)
-        return window.location.href = `${refURL}`
+        return res.redirect(`${refURL}`)
     })
     .catch(err => {
         console.log(err)
