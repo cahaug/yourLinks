c8661620d36ca0e4f139dce5b002ba9a59a07157
cahaug <caleahaug@gmail.com>
2020-03-12 02:31:04 -0700

    debugging

--- stats/stats-router.js
+++ stats/stats-router.js
@@ -7,14 +7,14 @@ statsRouter.get('/', async (req, res) => {
     const refURL = req.query.ref
     const entryId = req.query.eid
     const date = new Date().toISOString();
-    const dy = date[9, 10]
-    const mo = date[6, 7]
-    const yr = date[0, 4]
-    const hr = date[12, 13]
-    const mn = date[15, 16]
-    const sc = date[18, 19]
+    const dy = date.slice(9, 10)
+    const mo = date.slice(6, 7)
+    const yr = date.slice(0, 4)
+    const hr = date.slice(12, 13)
+    const mn = date.slice(15, 16)
+    const sc = date.slice(18, 19)
     const stat = { entryId, dy, mo, yr, hr, mn, sc }
-    console.log('stat', stat)
+    // console.log('stat', stat)
     return logAClick(stat)
     .then(result => {
         return window.location.href = `${refURL}`
