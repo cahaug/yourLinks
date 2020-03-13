bd4ba28787add14c227a1c1d2d8004d405969210
cahaug <caleahaug@gmail.com>
2020-03-13 01:48:32 -0700

    trying another thing

--- database/queries.js
+++ database/queries.js
@@ -35,7 +35,7 @@ module.exports = {
 
     
     getEntries2(userId){
-        return knex('entries').where("userId", userId).leftJoin('stats', 'entries.entryId', 'stats.entryId').select('stats.entryId').count().groupBy('stats.entryId').orderBy('stats.entryId', 'asc').select('entries.description')
+        return knex('entries').where("userId", userId).leftJoin('stats', 'entries.entryId', 'stats.entryId').select('stats.entryId').count().groupBy('stats.entryId').orderBy('stats.entryId', 'asc')
     },
 
     // getEntries3(userId){
