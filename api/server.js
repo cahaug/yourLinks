ced906a5e5b4de368f725dcdab4c163bdca2c1f7
cahaug <caleahaug@gmail.com>
2020-03-18 09:46:36 -0700

    perhaps some progress fixing cors

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ const { getEntries } = require('../database/queries.js');
 server.use(function (req, res, next) {
     if (req.headers.origin.endsWith('link-in-bio.netlify.com')) {
     //   res.setHeader('Access-Control-Allow-Origin', 'http://' + req.headers.origin)
-      res.setHeader('Access-Control-Allow-Origin', 'https://' + req.headers.origin)
+      res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type')
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
     }
