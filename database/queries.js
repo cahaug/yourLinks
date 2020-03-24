59d65a3202f71f48a306f3ffcdcf0df8fe9b8b71
cahaug <caleahaug@gmail.com>
2020-03-23 22:23:23 -0700

    trying to ascertain why last two values are juxtaposed

--- database/queries.js
+++ database/queries.js
@@ -35,7 +35,7 @@ module.exports = {
 
     
     getEntries2(userId){
-        return knex('entries').where("userId", userId).orderBy('entryId', 'asc').leftJoin('stats', 'entries.entryId', 'stats.entryId').select('stats.entryId').count().groupBy('entries.entryId').orderBy('stats.entryId', 'asc');
+        return knex('entries').where("userId", userId).orderBy('entryId', 'asc').leftJoin('stats', 'entries.entryId', 'stats.entryId').select('stats.entryId').count().groupBy('stats.entryId').orderBy('entries.entryId', 'asc');
     },
 
     listByCustomURL(customURL){
