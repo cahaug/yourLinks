562411f2848b8bdfbdd644cee46815ee59a86b46
cahaug <caleahaug@gmail.com>
2020-03-29 13:37:20 -0700

    fixing sql query

--- database/queries.js
+++ database/queries.js
@@ -39,7 +39,7 @@ module.exports = {
     },
 
     listByCustomURL(customURL){
-        return knex('lists').where("customURL", customURL);
+        return knex('lists').where("customURL", customURL).join('entries', 'lists.listId', 'entries.listId').orderBy('entries.entryId', 'asc');
     },
     // join('entries', 'lists.listId', 'entries.listId')
     checkIfCustomURLAvailable(customURL){
