13aa8f35beb9b4ffbd80fc21238d15644fbb6b24
cahaug <caleahaug@gmail.com>
2020-03-31 17:39:58 -0700

    trying for entry editor sql fix

--- entries/entries-router.js
+++ entries/entries-router.js
@@ -85,7 +85,7 @@ entriesRouter.put('/replaceEntry', async (req, res) => {
         res.header('Access-Control-Allow-Methods', 'GET, POST,  PUT, DELETE, OPTIONS')
         return res.status(200).json(result);
     })
-    .catch(err => res.status(500).json(err))
+    .catch(err => {console.log(err); res.status(500).json(err)})
 })
 
 
