912b058b5d5c9cdd18becaf32f55f9f9f4b45b06
cahaug <caleahaug@gmail.com>
2020-04-18 20:11:32 -0700

    working on creating multiple lists

--- lists/lists-router.js
+++ lists/lists-router.js
@@ -46,7 +46,7 @@ listsRouter.post('/new', async (req, res) => {
                 res.header('Access-Control-Allow-Origin', '*')
                 res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type')
                 res.header('Access-Control-Allow-Methods', 'GET, POST,  PUT, DELETE, OPTIONS')
-                res.status(200).json(list[-1]);
+                res.status(200).json(list);
             })
     })
     .catch(err => res.status(500).json(err));
