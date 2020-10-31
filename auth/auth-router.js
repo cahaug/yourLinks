0aa7f8df2f56dc5f4dee1b41f211aaadccf1063f
Cale Haug <caleahaug@gmail.com>
2020-10-31 00:06:18 -0700

    trying to fixing undefined pw

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -72,7 +72,9 @@ authRouter.post('/login', async (req, res) => {
     return singleUserForLogin(email)
       .first()
       .then(async user => {
-        if (user && bcrypt.compareSync(req.body.password, user[0].password)) {
+        console.log('user', user, user[0], user[0].password)
+        console.log('pw slice', password.slice(0,3), req.body.password.slice(0,3))
+        if (user && bcrypt.compareSync(password, user[0].password)) {
           // a jwt should be generated
           const token = generateToken(user[0]);
           // console.log('token', token);
