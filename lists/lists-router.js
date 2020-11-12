8ad87637e018163276a2b119312d1b5cd44afa2f
Cale Haug <caleahaug@gmail.com>
2020-11-12 11:39:18 -0700

    aight this the backend friendly version lmao

--- lists/lists-router.js
+++ lists/lists-router.js
@@ -247,9 +247,27 @@ listsRouter.put('/changeProfilePicture', restricted, async (req, res) => {
 listsRouter.put('/uploadProfilePicture', restricted, async (req, res) => {
     const {userId, imageString} = req.body
     const {sub} = req.decodedToken
+
+    const dataURItoBlob = (dataURI) => {
+        // convert base64/URLEncoded data component to raw binary data held in a string
+        var byteString;
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+            byteString = atob(dataURI.split(',')[1]);
+        else
+            byteString = unescape(dataURI.split(',')[1]);
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        // write the bytes of the string to a typed array
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+        return new Blob([ia], {type:mimeString});
+    }
+
     try {
         if(sub == userId && imageString){
-            const blob = await fetch(imageString).then(res => res.blob());
+            const blob = dataURItoBlob(imageString)
             const formData = new FormData()
             formData.append('file@', blob)
             formData.append('album', 'link-in.bio')
