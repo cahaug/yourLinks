017d78bce98dda9d737d75840435282bb0969db7
Cale Haug <caleahaug@gmail.com>
2020-12-11 19:29:16 -0700

    babies skin is dry and healthy

--- database/queries.js
+++ database/queries.js
@@ -221,7 +221,7 @@ module.exports = {
     },
 
     mostPop(){
-        return knex('pageViews').distinct('pageViews.listId').count().groupBy('pageViews.listId').join('lists', 'pageViews.listId', 'lists.listId').select('lists.customURL')
+        return knex('pageViews').distinct('pageViews.listId').count().groupBy('pageViews.listId').join('lists', 'pageViews.listId', 'lists.listId')
     },
 
     checkRecentlyAttempted(email){
