1dd65c9145a2a4828d32ef2d21be1fc8edbadfb7
Cale Haug <caleahaug@gmail.com>
2020-12-11 19:34:19 -0700

    this won't work

--- database/queries.js
+++ database/queries.js
@@ -221,7 +221,7 @@ module.exports = {
     },
 
     mostPop(){
-        return knex('pageViews').distinct('pageViews.listId').count().groupBy('pageViews.listId').join('lists', 'pageViews.listId', 'lists.listId')
+        return knex('pageViews').distinct('pageViews.listId').count().groupBy('pageViews.listId').orderBy('count', 'desc').join('lists', 'pageViews.listId', 'lists.customURL')
     },
 
     checkRecentlyAttempted(email){
