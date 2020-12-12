d1a114ecbbebb218900b6707f4197e9a51e3aa4f
Cale Haug <caleahaug@gmail.com>
2020-12-11 19:38:59 -0700

    remove dat

--- database/queries.js
+++ database/queries.js
@@ -221,7 +221,7 @@ module.exports = {
     },
 
     mostPop(){
-        return knex('pageViews').distinct('pageViews.listId').count().groupBy('pageViews.listId').orderBy('count', 'desc').join('lists', 'pageViews.listId', 'lists.customURL')
+        return knex('pageViews').distinct('pageViews.listId').count().groupBy('pageViews.listId').join('lists', 'pageViews.listId', 'lists.customURL')
     },
 
     checkRecentlyAttempted(email){
