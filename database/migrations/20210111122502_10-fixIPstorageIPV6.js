6511681ffe922fb6c09718c49ca06adcf159bf0d
Cale Haug <caleahaug@gmail.com>
2021-01-11 12:41:02 -0700

    reattempt migration big bruh off f moment luckily avoided by big brother PGSQL

--- database/migrations/20210111122502_10-fixIPstorageIPV6.js
+++ database/migrations/20210111122502_10-fixIPstorageIPV6.js
@@ -1,14 +1,20 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema
-    .alterTable('users', user => {
-        user.string('userIP', 130).alter();
+    .alterTable('stats', entry => {
+        entry.string('userIP', 50).alter();
+    })
+    .alterTable('pageViews', entry => {
+        entry.string('userIP', 50).alter();
     });
 };
 
 exports.down = function(knex, Promise) {
     return knex.schema
-    .alterTable('users', user => {
-        user.string('userIP', 16).alter();
+    .alterTable('stats', entry => {
+        entry.string('userIP', 16).alter();
+    })
+    .alterTable('pageViews', entry => {
+        entry.string('userIP', 16).alter();
     });
 };
