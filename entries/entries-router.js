71dcf0a4377459521069841cd318ff8314ed4988
Cale Haug <caleahaug@gmail.com>
2021-01-28 21:28:18 -0700

    back to working

--- entries/entries-router.js
+++ entries/entries-router.js
@@ -233,7 +233,8 @@ entriesRouter.post('/uploadPhoto/:userId', hostNameGuard, restricted, check('use
         const userId = parseInt(req.params.userId, 10)
         if(sub === userId){
             console.log('req.file', req.files.myImage)
-            const myFile = new File({buffer: req.files.myImage.data, name:req.files.myImage.name, type:req.files.myImage.mimetype})
+            // const myFile = new File({buffer: req.files.myImage.data, name:req.files.myImage.name, type:req.files.myImage.mimetype})
+            // console.log('file', myFile)
             // function dataURItoBlob(dataURI) {
             //     var binary = atob(dataURI.split(',')[1]);
             //     var array = [];
@@ -242,18 +243,18 @@ entriesRouter.post('/uploadPhoto/:userId', hostNameGuard, restricted, check('use
             //     }
             //     return new Blob([new Uint8Array(array)], {type: `${req.files.myImage.mimetype}`});
             // }
-            const myImageActual = fs.createReadStream(myFile)
-            // const myimage = fs.createReadStream(req.files.myImage.tempFilePath)
-            const formData = new FormData()
-            const girlSecret = process.env.GIRLSECRET
-            formData.append('secret', girlSecret)
-            formData.append('myImage', myImageActual, `${req.files.myImage.name}`)
+            // const myImageActual = fs.createReadStream(myFile)
+            const myimage = fs.createReadStream(req.files.myImage.tempFilePath)
+            // const formData = new FormData()
+            // const girlSecret = process.env.GIRLSECRET
+            // formData.append('secret', girlSecret)
+            // formData.append('myImage', myImageActual, `${req.files.myImage.name}`)
             
-            const cleanImage = await axios.post('http://mw-im.pro/i/processThis', formData, formData.getHeaders())
-            console.log('cleanImage.data',cleanImage.data)
-            console.log('cleanImage data length', cleanImage.length, cleanImage.data.length)
-            const cleanedmyimage = fs.createReadStream(cleanImage.data)
-            imageshack.upload(cleanedmyimage, async function(err, filejson){
+            // const cleanImage = await axios.post('http://mw-im.pro/i/processThis', formData, formData.getHeaders())
+            // console.log('cleanImage.data',cleanImage.data)
+            // console.log('cleanImage data length', cleanImage.length, cleanImage.data.length)
+            // const cleanedmyimage = fs.createReadStream(cleanImage.data)
+            imageshack.upload(myimage, async function(err, filejson){
                 if(err){
                     console.log(err);
                 }else{
