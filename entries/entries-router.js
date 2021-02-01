5a08263001862fe70099de2ebd28837d1927edba
cahaug <caleahaug@gmail.com>
2021-02-01 06:20:09 +0000

    no conf

--- entries/entries-router.js
+++ entries/entries-router.js
@@ -32,7 +32,7 @@ entriesRouter.post('/new', hostNameGuard, restricted, body('userId').notEmpty().
             // oh how nice to be just a droplet in the digital ocean *music emoji*
             let isURLmalicious = null
             if(referencingURL != null && referencingURL.trim().indexOf('http') == 0){
-                const safeURLCheck = await axios.post('https://mw-im.pro/h/a', { referencingURL:referencingURL, secret:process.env.BOYSECRET })
+                const safeURLCheck = await axios.post('http://10.124.0.2/h/a', { referencingURL:referencingURL, secret:process.env.BOYSECRET })
                 isURLmalicious = safeURLCheck.data.malicious
             } else {
                 //isnotmalicious=false
